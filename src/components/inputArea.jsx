584aeb169e6f3d65bb75c7cb918a510c5846d5c7
Partha Pratim Sarma <parthasarma34@gmail.com>
2021-06-07 12:11:33 +0530

    Initial commit

---
+++ src/components/inputArea.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../style.css"
+
+function inputArea() {
+    return (
+        <div className="input-main">
+            <h1>Hello from input</h1>
+        </div>
+    )
+}
+
+export default inputArea
---
+++ src/components/resultArea.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../style.css"
+
+function resultArea() {
+    return (
+        <div className="result-main">
+            <h1>Hello from result</h1>
+        </div>
+    )
+}
+
+export default resultArea
